5653d1b2c42681093c1a43a7bebcce12d24a3904
Cheung, Kin Ching | Ken | BSD <kinching.a.cheung@rakuten.com>
2025-02-13 19:18:35 +0900

    remove build folder

--- build/constants/KafkaConstant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KAFKA_SASL_MECHANISM = exports.KAFKA_SECURITY_PROTOCOL = void 0;
-const KAFKA_SECURITY_PROTOCOL = "sasl_ssl";
-exports.KAFKA_SECURITY_PROTOCOL = KAFKA_SECURITY_PROTOCOL;
-const KAFKA_SASL_MECHANISM = "PLAIN";
-exports.KAFKA_SASL_MECHANISM = KAFKA_SASL_MECHANISM;
--- build/utils/messageUtil.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateMessage = void 0;
-const generateMessage = (changeFlags) => {
-    return Object.keys(changeFlags).filter(key => changeFlags[key]).map(key => ({
-        value: key
-    }));
-};
-exports.generateMessage = generateMessage;
